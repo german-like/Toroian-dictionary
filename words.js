fe3babcefba6aaa8f51d714422c457c322eed696
german-like <germaaaaaan.sub@gmail.com>
2025-11-18 19:31:49 +0900

    Create words.js

---
+++ words.js
@@ -0,0 +1,14 @@
+const WORDS = [
+  {
+    word: "salan",
+    meaning: "こんにちは（架空言語）",
+    pos: "挨拶語",
+    example: "Salan veo!（やあ！）"
+  },
+  {
+    word: "luma",
+    meaning: "光",
+    pos: "名詞",
+    example: "Luma eni falen.（光が空に満ちる）"
+  }
+];
