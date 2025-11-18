f1cbb2fcf8ad91789c5a7e213491eb84c73555a3
german-like <germaaaaaan.sub@gmail.com>
2025-11-18 22:56:48 +0900

    Update script.js

--- script.js
+++ script.js
@@ -69,16 +69,16 @@ function displayWords(wordArray) {
 
     resultArea.innerHTML += `
       <div class="word-card">
-        <h2>${w.word}</h2>
-        ${ipa}
-        ${classInfo}
-        <h3>意味</h3>${meaningsHtml}
-        <h3>変化形</h3>${conjugations}
-        <p><b>類義語：</b> ${synonyms}</p>
-        <p><b>対義語：</b> ${antonyms}</p>
-        <p><b>タグ：</b> ${tags}</p>
-        <p><b>最終更新：</b> ${updatedAt}</p>
-      </div>
+        <h2>${w.word}</h2><br>
+        ${ipa}<br>
+        ${classInfo}<br>
+        <h3>意味</h3>${meaningsHtml}<br>
+        <h3>変化形</h3>${conjugations}<br>
+        <p><b>類義語：</b> ${synonyms}</p><br>
+        <p><b>対義語：</b> ${antonyms}</p><br>
+        <p><b>タグ：</b> ${tags}</p><br>
+        <p><b>最終更新：</b> ${updatedAt}</p><br>
+      </div><br>
     `;
   });
 }
