1f9adbadc8bf29dc067bd65fcb0106bbb0a17115
german-like <germaaaaaan.sub@gmail.com>
2025-11-19 19:12:09 +0900

    Update words.js

--- words.js
+++ words.js
@@ -26,9 +26,41 @@ const WORDS = [
       "奪格": "med",
       "具格": "man",
     },
-    "synonyms": ["wi"],
-    "antonyms": ["żig"],
+    "synonyms": ["żig"],
+    "antonyms": [""],
     "tags": ["代名詞"],
     "updatedAt": "2025-11-18"
   }
+  {
+    "word": "żig",
+    "pronunciation": {
+      "ipa": "ˈdʑiɡ"
+    },
+    "class": {
+      "pos": "代名詞"
+    },
+    "meanings": [
+      {
+        "gloss": "私たち",
+        "description": "一人称複数代名詞",
+        "example": {
+          "sentence": "Żig eź šebo.",
+          "translation": "私たちは元気です。"
+        }
+      }
+    ],
+    "conjugations": {
+      "主格": "żig",
+      "対格": "żeg",
+      "与格": "żigi",
+      "属格": "żoga",
+      "処格": "żerr",
+      "奪格": "żed",
+      "具格": "żyb",
+    },
+    "synonyms": ["Ja"],
+    "antonyms": [""],
+    "tags": ["代名詞"],
+    "updatedAt": "2025-11-19"
+      }
 ];
