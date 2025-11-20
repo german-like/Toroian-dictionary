c7619aaf0039abaf938124d249056394c26bf663
german-like <germaaaaaan.sub@gmail.com>
2025-11-20 20:29:28 +0900

    Update words.js

--- words.js
+++ words.js
@@ -62,5 +62,45 @@ const WORDS = [
     "antonyms": [""],
     "tags": ["代名詞"],
     "updatedAt": "2025-11-19"
+    }
+    {
+    "word": "Ja",
+    "pronunciation": {
+      "ipa": "ˈja"
+    },
+    "class": {
+      "pos": "代名詞"
+    },
+    "meanings": [
+      {
+        "gloss": "私",
+        "description": "一人称単数代名詞",
+        "example": {
+          "sentence": "Ja din Kaar.",
+          "translation": "私はカールです。"
+        }
       }
+    ],
+    "conjugations": {
+      "主格": "Ja",
+      "対格": "me",
+      "与格": "mi",
+      "属格": "moi",
+      "処格": "mus",
+      "奪格": "med",
+      "具格": "man",
+    },
+    "synonyms": ["żig"],
+    "antonyms": [""],
+    "tags": ["代名詞"],
+    "updatedAt": "2025-11-18"
+    },
+    {
+    "word": "żig",
+    "pronunciation": {
+      "ipa": "ˈdʑiɡ"
+    },
+    "class": {
+      "pos": "代名詞"
+    },
 ];
